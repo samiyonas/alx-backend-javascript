226ae4476e81544b16efde0853d9622e3ed26fb7
samiyonas <samuel.x.yonas@gmail.com>
2024-08-08 14:10:28 +0300

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock (trueorFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
+  if (trueorFalse) {
     const task = true; // eslint-disable-line no-unused-vars
     const task2 = false; // eslint-disable-line no-unused-vars
   }
