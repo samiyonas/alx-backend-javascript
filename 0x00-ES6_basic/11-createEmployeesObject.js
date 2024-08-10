6a1f61b73c28bf7cb54ccda6c981da0c204c2ae4
samiyonas <samuel.x.yonas@gmail.com>
2024-08-10 16:33:02 +0300

    iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const ar = [];
-  for (const i of employees){
-    ar.push(i);
-  }
   const dep = {
-    departmentName: ar,
+    [departmentName]: employees,
   }
 
   return dep;
