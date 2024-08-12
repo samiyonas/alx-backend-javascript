868fbebfeab28399d1820f4faad36a0756d03326
samiyonas <samuel.x.yonas@gmail.com>
2024-08-12 20:54:06 +0300

    promise then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,14 +2,14 @@
 
 export default function handleResponseFromAPI(promise) {
   return promise
-  .then(() => ({
-    status: 200,
-    body: 'success',
-  }))
-  .catch(() => {
-    new Error();
-  })
-  .finally(() => {
-    console.log('Got a response from the API');
-  })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    })
 }
