772bc678f017e285d80519f312266829a4d2fbe4
samiyonas <samuel.x.yonas@gmail.com>
2024-08-13 11:45:36 +0300

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,13 @@
-export default function loadBalancer(chinaDownload, USDownload) {
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .race([chinaDownload, USDownload])
-    .then((res) => res);
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
