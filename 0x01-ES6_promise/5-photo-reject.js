a80e160a1246fad7bfaf46e2c835b27a30cfbd47
samiyonas <samuel.x.yonas@gmail.com>
2024-08-13 11:23:59 +0300

    reject the promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 export default function uploadPhoto(filename) {
-  return Promise.reject({
-    Error: `${filename} cannot be processed`,
-  });
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`)
+  );
 }
