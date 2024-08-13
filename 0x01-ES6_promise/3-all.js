f42530597da3c86e2bfe50507d0001acb9e46714
samiyonas <samuel.x.yonas@gmail.com>
2024-08-13 11:05:05 +0300

    return all resolved at once

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
@@ -7,5 +7,5 @@ export default function handleProfileSignup() {
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch(() => console.log("Signup system offline"))
+    .catch(() => console.log('Signup system offline'));
 }
