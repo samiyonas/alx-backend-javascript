47934a9db5805eccef74ffe37105a86a994a1f4b
samiyonas <samuel.x.yonas@gmail.com>
2024-08-20 16:26:59 +0300

    class implementation with getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     this.name = name;
-    this.length = length
+    this.length = length;
     this.students = students;
   }
 
